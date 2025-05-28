a462ffb5e69e0849e29d282838c08a26e13d3a50
kaitlyn <kaitlynohw@gmail.com>
2025-05-29 04:04:34 +0800

    Quiz and Results page

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,5 +1,50 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const questions = [
+        {
+            question: "What is your skin type?",
+            options: ["Oily", "Dry", "Combination", "Sensitive"],
+        },
+        {
+            question: "What is your biggest skin concern?",
+            options: ["Acne", "Aging", "Oiliness", "Dryness"],
+        },
+        {
+            question: "What is your skincare budget?",
+            options: ["$10-$50", "$50-$100", "$100-$200", "Budget is not a concern"],
+        },
+    ];
+
 function Quiz() {
-    return<p>quiz</p>;
+    const [current, setCurrent] = useState(0);
+    const [answers, setAnswers] = useState<string[]>([]);
+    const navigate = useNavigate();
+
+    const handleAnswer = (option: string) => {
+        setAnswers([...answers, option]);
+
+        if (current + 1 < questions.length) {
+            setCurrent(current + 1);
+        } else {
+            navigate('/result', {state: { answers } });
+        }
+    };
+
+    const q = questions[current];
+
+    return(
+        <div style={{padding: '2rem'}}>
+            <h2>{q.question}</h2>
+            {q.options.map((option) => (
+                <button
+                key={option}
+                onClick={() => handleAnswer(option)} 
+                style={{ display: 'block', margin: '1rem 0' }}>{option}
+                </button>
+            ))}
+        </div>
+    );
 }
 
 export default Quiz;
--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,5 +1,19 @@
 function Result() {
-    return<p>result</p>;
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>Your Results:</h1>
+            <h2>Step 1: Cleanser</h2>
+            <p>We recommend Dr. Loretta's Gentle Hydrating Cleanser.</p>
+            <h2>Step 2: Toner</h2>
+            <p>We recommend CeraVe Hydrating Toner.</p>
+            <h2>Step 3: Serum</h2>
+            <p>We recommend The Ordinary Hyaluronic Acid.</p>
+            <h2>Step 4: Moisturiser</h2>
+            <p>We recommend La Mer's The Moisturizing Soft Cream Duo.</p>
+            <h2>Step 5: Sunscreen</h2>
+            <p>We recommend ANESSA Perfect UV Sunscreen Skincare Gel. </p>
+        </div>
+    );
 }
 
 export default Result;
